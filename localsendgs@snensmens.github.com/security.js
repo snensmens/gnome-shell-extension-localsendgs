6c3eab9d8c6ca38ee9f9a97aa4d285b1d58769ba
Clemens Weglau <snensmens@users.noreply.github.com>
2025-03-08 14:17:52 +0100

    add function to create certificate

--- localsendgs@snensmens.github.com/security.js
+++ localsendgs@snensmens.github.com/security.js
@@ -4,31 +4,53 @@ import GLib from "gi://GLib";
 Gio._promisify(Gio.Subprocess.prototype, "communicate_utf8_async", "communicate_utf8_finish");
 Gio._promisify(Gio.File.prototype, "replace_contents_async", "replace_contents_finish");
 
-export async function createPrivateKey({ path, cancellable }) {
-  const process = new Gio.Subprocess({
-    argv: ["certtool", "--generate-privkey", "--no-text"],
-    flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
-  });
-  process.init(cancellable);
 
-  const cancelId = cancellable?.connect(() => process.force_exit());
+async function createFileFromSubprocess({ args, path, cancellable }) {
+  try {
+    const process = new Gio.Subprocess({
+      argv: args,
+      flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+    });
+    process.init(cancellable);
 
-  const [stdout, stderr] = await process.communicate_utf8_async(null, cancellable);
+    const cancelId = cancellable?.connect(() => process.force_exit());
+    const [stdout, stderr] = await process.communicate_utf8_async(null, cancellable);
 
-  cancellable?.disconnect(cancelId);
+    cancellable?.disconnect(cancelId);
 
-  if (process.get_exit_status() === 0) {
-    const keyFile = Gio.File.new_for_path(path);
+    if (process.get_exit_status() !== 0) {
+      throw stderr.trim();
+    }
 
-    await keyFile.replace_contents_async(
+    const file = Gio.File.new_for_path(path);
+    await file.replace_contents_async(
       new TextEncoder().encode(stdout.trim()),
       null, false, Gio.FileCreateFlags.NONE,
       cancellable,
     );
-
-    print("created new private key");
     return true;
   }
+  catch (error) {
+    console.error(error);
+  }
 
-  console.error(`failed to create private key: ${stderr.trim()}`);
+  return false;
+}
+
+
+export async function createPrivateKey({ path, cancellable }) {
+  return await createFileFromSubprocess({
+    args: ["certtool", "--generate-privkey", "--no-text"],
+    path: path,
+    cancellable: cancellable,
+  });
+}
+
+
+export async function createCertificate({ path, key, template, cancellable }) {
+  return await createFileFromSubprocess({
+    args: ["certtool", "-s", "--load-privkey", key, "--template", template, "--no-text", "--verbose"],
+    path: path,
+    cancellable: cancellable,
+  });
 }
